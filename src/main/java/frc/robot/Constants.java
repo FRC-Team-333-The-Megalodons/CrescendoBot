3dcc3a998db256f37f04bcd58a813c5ae710ea83
Emmanuel <ejeffers22@gmail.com>
2024-01-16 10:03:57 -0500

    started adding constants for modules

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,5 +4,39 @@
 
 package frc.robot;
 
-/** Add your docs here. */
-public class Constants {}
+import com.revrobotics.CANSparkBase.IdleMode;
+
+public class Constants {
+
+    // Spark Flex Config
+    public static final IdleMode driveMotorIdleMode = IdleMode.kCoast;
+    public static final IdleMode steerMotorIdleMode = IdleMode.kBrake;
+
+    public static final class ModuleConstants {
+
+        public static final boolean encoderInvert = true;
+
+        // Drive motor conversion factors
+        public static final double driveMotorFreeSpeed = 0.0; // RPMs
+    }
+
+    /* Mod 0: Front Left */ 
+    public static final int frontLeftDriveID = 11;
+    public static final int frontLeftSteerID = 21;
+    public static final int frontLeftEncoderID = 31;
+
+    /* Mod 0: Front Left */ 
+    public static final int frontRightDriveID = 12;
+    public static final int frontRightSteerID = 22;
+    public static final int frontRightEncoderID = 32;
+
+    /* Mod 0: Front Left */ 
+    public static final int rearLeftDriveID = 13;
+    public static final int rearLeftSteerID = 23;
+    public static final int rearLeftEncoderID = 33;
+
+    /* Mod 0: Front Left */ 
+    public static final int rearRightDriveID = 14;
+    public static final int rearRightSteerID = 24;
+    public static final int rearRightEncoderID = 34;
+}
