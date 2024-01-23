6ef0f3b9dae3cc960c4744814448728011291175
amina7mona <114757533+amina7mona@users.noreply.github.com>
2024-01-23 12:39:36 -0500

    intake logic added

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,5 +4,49 @@
 
 package frc.robot.subsystems;
 
-/** Add your docs here. */
-public class Intake {}
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private CANSparkFlex intakeMotor;
+
+  /** Creates a new Intake. */
+  public Intake() {
+    intakeMotor = new CANSparkFlex(1, MotorType.kBrushless);
+    intakeMotor.setIdleMode(IdleMode.kCoast);
+  }
+
+  public void intake() { intakeMotor.set(-0.15); } // intake speed
+  
+
+  public void outake() { intakeMotor.set(0.15); } // outake speed 
+
+  public void notake() { intakeMotor.set(0); }
+
+  public boolean intakeAutoDone() {
+    if (intakeMotor.getEncoder().getPosition() <= -0) { // intake encoder position 
+      return true;
+    }
+    return false;
+  }
+
+  public boolean outakeAutoDone() {
+    if (intakeMotor.getEncoder().getPosition() >= 0) { // outake encoder positon 
+      return true;
+    }
+    return false;
+  }
+
+  public void resetIntakeEncoder() { intakeMotor.getEncoder().setPosition(0); }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Intake encoer", intakeMotor.getEncoder().getPosition());
+  }
+}
