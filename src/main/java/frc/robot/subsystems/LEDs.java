6b3869f585d1925858416f857cc7d2bb968754cf
amina7mona <114757533+amina7mona@users.noreply.github.com>
2024-01-25 14:42:01 -0500

    sleleton done

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// isn't extentiated in the RobotContainer yet
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
@@ -16,7 +17,7 @@ public class LEDs extends SubsystemBase {
     m_led = new AddressableLED(3);
     m_ledBuffer = new AddressableLEDBuffer(60);
     m_led.setLength(m_ledBuffer.getLength());
-
+    m_led.setData(m_ledBuffer);
     m_led.start();
 
   }
@@ -24,36 +25,42 @@ public class LEDs extends SubsystemBase {
   public void setColorLED() {
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       m_ledBuffer.setRGB(i, 032, 564, 543);
-    }
+    } m_led.setData(m_ledBuffer);
   }
+
   public void whatLightLED() {
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
         m_ledBuffer.setLED(i, Color.kBlueViolet);
-    }
+    } m_led.setData(m_ledBuffer);
   }
+
   public void royalBlueLED() {
     for (var i = 0; i< m_ledBuffer.getLength(); i++) {
         m_ledBuffer.setRGB(i, 0, 32, 135);
-    }
+    } m_led.setData(m_ledBuffer);
   }
+
   public void orangeLED() {
     for (var i = 0; i< m_ledBuffer.getLength(); i++) {
         m_ledBuffer.setRGB(i, 255, 127, 0);
-    }
+    } m_led.setData(m_ledBuffer);
   }
+
   public void yellowLED() {
     for (var i = 0; i< m_ledBuffer.getLength(); i++) {
         m_ledBuffer.setRGB(i, 230, 223, 0);
-    }
+    } m_led.setData(m_ledBuffer);
   }
+
   public void greenLED() {
     for (var i = 0; i< m_ledBuffer.getLength(); i++) {
         m_ledBuffer.setRGB(i, 0, 250, 0);
-    }
+    } m_led.setData(m_ledBuffer);
   }
+  
   public void redLED() {
     for (var i = 0; i< m_ledBuffer.getLength(); i++) {
-        m_ledBuffer.setRGB(i, 250, 0, 0);
-    }
+        m_ledBuffer.setRGB(i, 250  , 0, 0);
+    } m_led.setData(m_ledBuffer);
   }
 }
