cafa785248388ff9d3cc2a88138627b2015eadd4
Supreza <100383264+HakimB2007@users.noreply.github.com>
2024-03-02 23:08:59 -0500

    intake improted

--- src/main/java/frc/robot/commands/AutoIntake.java
+++ src/main/java/frc/robot/commands/AutoIntake.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.WristConstants;
+import frc.robot.commands.basic.RunIntake;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Trolley;
 import frc.robot.subsystems.Wrist;
